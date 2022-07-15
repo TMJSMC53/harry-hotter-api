fdccdfceeaf44485425e20da22b88a17141427ec
Triola Jackson <triolajackson@gmail.com>
2022-07-14 23:54:23 -0500

    added more characters and updated character info

--- server.js
+++ server.js
@@ -8,8 +8,25 @@ app.use(cors());
 app.use(express.json());
 
 const characters = {
+  "albus dumbledore": {
+    name: "Albus Percival Wulfric Brian Dumbledore",
+    birthday: "1881",
+    house: "Gryffindor",
+    patronus: "Phoenix",
+    boggart: "The corpse of his sister, Ariana",
+    wand: "Elder Wand",
+    parents: "Percival Dumbledore and Kendra Dumbledore",
+    bloodStatus: "Half-blood",
+    children: [
+      "James Sirius Potter",
+      "Lily Luna Potter",
+      "Albus Severus Potter",
+    ],
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/f/fe/Dumbledore_and_Elder_Wand.JPG",
+  },
   "harry potter": {
-    name: "Harry Potter",
+    name: "Harry James Potter",
     birthday: "July 31, 1980",
     house: "Gryffindor",
     patronus: "Stag",
@@ -27,7 +44,7 @@ const characters = {
   },
 
   "hermione granger": {
-    name: "Hermione Granger",
+    name: "Hermione Jean Granger",
     birthday: "September 19, 1979",
     house: "Gryffindor",
     patronus: "Otter",
@@ -36,24 +53,26 @@ const characters = {
     wand: "Vine",
     parents: "Mr. Granger and Mrs. Granger",
     bloodStatus: "Muggle-born",
+    children: ["Rose Granger-Weasley", "Hugo Granger-Weasley"],
     image:
       "https://upload.wikimedia.org/wikipedia/en/d/d3/Hermione_Granger_poster.jpg",
   },
   "ron weasley": {
-    name: "Ron Weasley",
+    name: "Ronald Bilius Weasley",
     birthday: "March 1, 1980",
     house: "Gryffindor",
     patronus: "Jack Russell Terrier",
     boggart: "Aragog",
     wand: "Willow",
     parents: "Arthur Weasley and Molly Weasley",
+    children: ["Rose Granger-Weasley", "Hugo Granger-Weasley"],
     bloodStatus: "Pure-blood",
     image:
       "https://upload.wikimedia.org/wikipedia/en/5/5e/Ron_Weasley_poster.jpg",
   },
 
   "draco malfoy": {
-    name: "Draco Malfoy",
+    name: "Draco Lucius Malfoy",
     birthday: "June 5, 1980",
     house: "Slytherin",
     patronus: "Dragon",
@@ -62,8 +81,21 @@ const characters = {
     parents: "Lucius Malfoy and Narcissa Malfoy",
     children: ["Scorpius Hyperion Malfoy"],
     bloodStatus: "Pure-blood",
+    image: "https://upload.wikimedia.org/wikipedia/en/1/16/Draco_Mal.JPG",
+  },
+
+  "neville longbottom": {
+    name: "Neville Longbottom",
+    birthday: "July 30, 1980",
+    house: "Gryffindor",
+    patronus: "Stag",
+    boggart: "Dementor",
+    wand: "",
+    parents: "Frank Longbottom and Alice Longbottom",
+    children: ["Unknown"],
+    bloodStatus: "Pure-blood",
     image:
-      "https://upload.wikimedia.org/wikipedia/en/f/f9/Draco_Malfoy_poster.jpg",
+      "https://upload.wikimedia.org/wikipedia/en/7/75/Neville_Longbottom.jpeg",
   },
 
   unknown: {
