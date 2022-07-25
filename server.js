a79a5ed5145967aa462d9e04208b8e37afdf607c
Triola Jackson <triolajackson@gmail.com>
2022-07-25 11:52:43 -0500

    updated express.static public

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const connectionString = process.env.DB_STRING;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use("/public", express.static("public"));
 
 // const characters = {
 //   "albus dumbledore": {
