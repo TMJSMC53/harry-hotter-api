3bfca0e3808cedd6b22f42b4a43cc4b8eb1229ac
Triola Jackson <triolajackson@gmail.com>
2022-08-01 21:45:40 -0500

    update server.js file

--- server.js
+++ server.js
@@ -150,15 +150,16 @@ app.use(express.urlencoded({ extended: true }));
 
 // connect to ejs
 app.get("/", (req, res) => {
-  db.collection("character-info")
-    .find()
-    .toArray()
-    .then((data) => {
-      let characterList = data.map((item) => item.characterName);
-      console.log(characterList);
-      res.render("index.ejs", { info: characterList });
-    })
-    .catch((error) => console.log(error));
+  res.render("index.ejs", { info: [] });
+  // db.collection("character-info")
+  //   .find()
+  //   .toArray()
+  //   .then((data) => {
+  //     let characterList = data.map((item) => item.characterName);
+  //     console.log(characterList);
+  //     res.render("index.ejs", { info: characterList });
+  //   })
+  //   .catch((error) => console.log(error));
 });
 
 // Serving Up JSON / This is a network request
